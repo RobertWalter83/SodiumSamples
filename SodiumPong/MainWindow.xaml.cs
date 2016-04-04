a2501ab05767e7786ba63f3389991fe206e706b5
Robert Walter <Robert Walter>
2016-04-04 15:03:03 +0200

    made all functions static, filter key events, give UI element to GameObject (maybe bad)

--- SodiumPong/MainWindow.xaml.cs
+++ SodiumPong/MainWindow.xaml.cs
@@ -24,17 +24,14 @@ namespace SodiumPong
         const int dyBoardHalf = dyBoard / 2;
         #endregion
 
-        private readonly StreamSink<KeyEventArgs> sKeyEvents = new StreamSink<KeyEventArgs>();
-        private readonly StreamSink<RenderingEventArgs> sRenderEvents = new StreamSink<RenderingEventArgs>();
-
         public MainWindow()
         {
             InitializeComponent();
             this.cvs.Width = dxBoard;
             this.cvs.Height = dyBoard;
-            Rectangle rectP1 = RectanglePlayer();
-            Rectangle rectP2 = RectanglePlayer();
-            Ellipse ellipseBall = EllipseBall();
+            Rectangle viewP1 = RectanglePlayer();
+            Rectangle viewP2 = RectanglePlayer();
+            Ellipse viewBall = EllipseBall();
             Rectangle midLine = MidLine();
 
             TextBlock textScore1 = TextScore();
@@ -45,9 +42,9 @@ namespace SodiumPong
             this.cvs.Children.Add(textScore1);
             this.cvs.Children.Add(textScore2);
             this.cvs.Children.Add(textRunPause);
-            this.cvs.Children.Add(ellipseBall);
-            this.cvs.Children.Add(rectP1);
-            this.cvs.Children.Add(rectP2);
+            this.cvs.Children.Add(viewBall);
+            this.cvs.Children.Add(viewP1);
+            this.cvs.Children.Add(viewP2);
 
             Canvas.SetLeft(midLine, dxBoardHalf);
             Canvas.SetTop(midLine, 0);
@@ -63,51 +60,34 @@ namespace SodiumPong
 
             Transaction.RunVoid(() =>
             {
+                StreamSink<KeyEventArgs> sKeyEvents = new StreamSink<KeyEventArgs>();
+
                 KeyDown += (sender, args) => sKeyEvents.Send(args);
                 KeyUp += (sender, args) => sKeyEvents.Send(args);
 
+                StreamSink<RenderingEventArgs> sRenderEvents = new StreamSink<RenderingEventArgs>();
                 CompositionTarget.Rendering += (sender, args) => sRenderEvents.Send((RenderingEventArgs)args);
 
                 Stream<TimeSpan> sTicks = sRenderEvents.Collect(TimeSpan.Zero,
                     (re, t) => Tuple.Create(re.RenderingTime - t, re.RenderingTime));
 
-                Cell<GameState> cGameState = sKeyEvents.Accum(GameState.Paused, (keyEventArgs, gameState) =>
-                {
-                    if (keyEventArgs.Key == Key.Space && keyEventArgs.IsDown)
-                        return GameState.Running & ~gameState;
+                Cell<int> cDirP1 = CellDirPlayer(sKeyEvents, Key.W, Key.S);
+                Cell<int> cDirP2 = CellDirPlayer(sKeyEvents, Key.Up, Key.Down);
 
-                    return gameState;
-                });
+                Cell<GameState> cGameState = sKeyEvents.Filter(e => e.Key == Key.Space).Accum(GameState.Paused, UpdateGameState);
 
-                Cell<GameObject> cP1 = CellPlayer(PlayerInitial(new Point(20, dyBoardHalf)), sTicks, Key.W,
-                    Key.S, cGameState);
-
-                Cell<GameObject> cP2 = CellPlayer(PlayerInitial(new Point(dxBoard - 20, dyBoardHalf)), sTicks,
-                    Key.Up, Key.Down, cGameState);
+                Cell<GameObject> cP1 = CellPlayer(PlayerInitial(new Point(20, dyBoardHalf), viewP1), sTicks, cDirP1, cGameState);
+                Cell<GameObject> cP2 = CellPlayer(PlayerInitial(new Point(dxBoard - 20, dyBoardHalf), viewP2), sTicks, cDirP2, cGameState);
 
                 CellLoop<GameObject> cBall = new CellLoop<GameObject>();
                 Stream<GameObject> sBall = sTicks.Snapshot(cBall, cP1, cP2, cGameState, UpdateBall);
                 Cell<int> cScore1 = sBall.Accum(0, (ball, score) => ball.pos.X > dxBoard ? ++score : score);
                 Cell<int> cScore2 = sBall.Accum(0, (ball, score) => ball.pos.X <= 0 ? ++score : score);
-                cBall.Loop(sBall.Hold(BallInitial()));
+                cBall.Loop(sBall.Hold(BallInitial(viewBall)));
 
-                cP1.Listen(player =>
-                {
-                    Canvas.SetLeft(rectP1, player.pos.X);
-                    Canvas.SetTop(rectP1, player.pos.Y);
-                });
-
-                cP2.Listen(player =>
-                {
-                    Canvas.SetLeft(rectP2, player.pos.X);
-                    Canvas.SetTop(rectP2, player.pos.Y);
-                });
-
-                cBall.Listen(ball =>
-                {
-                    Canvas.SetLeft(ellipseBall, ball.pos.X);
-                    Canvas.SetTop(ellipseBall, ball.pos.Y);
-                });
+                cP1.Listen(Draw);
+                cP2.Listen(Draw);
+                cBall.Listen(Draw);
 
                 cScore1.Listen(score =>
                 {
@@ -126,32 +106,27 @@ namespace SodiumPong
             });
         }
 
-        private Cell<GameObject> CellPlayer(GameObject playerInitial, Stream<TimeSpan> sTicks, Key keyUp, Key keyDown, Cell<GameState> cGameState)
+        private static Cell<int> CellDirPlayer(Stream<KeyEventArgs> sKeyEvents, Key keyUp, Key keyDown)
+        {
+            Stream<int> sDirP1Left = sKeyEvents.Filter(e => e.Key == keyUp).Map(e => e.IsDown ? -1 : 0);
+            Stream<int> sDirP1Right = sKeyEvents.Filter(e => e.Key == keyDown).Map(e => e.IsDown ? 1 : 0);
+            return sDirP1Left.OrElse(sDirP1Right).Hold(0);
+        }
+
+        private static GameState UpdateGameState(KeyEventArgs e, GameState gameState)
+        {
+            return e.IsDown ? GameState.Running & ~gameState : gameState;
+        }
+
+        private static Cell<GameObject> CellPlayer(GameObject playerInitial, Stream<TimeSpan> sTicks, Cell<int> cDir, Cell<GameState> cGameState)
         {
             CellLoop<GameObject> cPlayer = new CellLoop<GameObject>();
-            Cell<int> cDir = sKeyEvents.Accum(0, (e, dir) => UpdateDir(dir, keyUp, keyDown, e));
             Stream<GameObject> sPlayer = sTicks.Snapshot(cPlayer, cDir, cGameState, UpdatePlayer);
             cPlayer.Loop(sPlayer.Hold(playerInitial));
             return cPlayer;
         }
 
-        private GameObject PlayerInitial(Point pointInitial)
-        {
-            return new GameObject(pointInitial, new Vector(0, 0));
-        }
-
-        private static int UpdateDir(int dir, Key keyUp, Key keyDown, KeyEventArgs e)
-        {
-            if (e.Key != keyUp && e.Key != keyDown)
-                return dir;
-
-            if (e.IsDown)
-                return e.Key == keyUp ? -1 : 1;
-
-            return 0;
-        }
-
-        private GameObject UpdatePlayer(TimeSpan timeSpan, GameObject player, int dir, GameState gameState)
+        private static GameObject UpdatePlayer(TimeSpan timeSpan, GameObject player, int dir, GameState gameState)
         {
             if (gameState == GameState.Paused)
                 return player;
@@ -159,46 +134,42 @@ namespace SodiumPong
             return Clamp(PhysicsUpdate(timeSpan, PlayerVeloUpdate(player, dir)));
         }
 
-        private GameObject Clamp(GameObject player)
+        private static GameObject Clamp(GameObject player)
         {
-            return new GameObject(new Point(player.pos.X, player.pos.Y.Clamp(25, 375)), player.vel);
+            return new GameObject(new Point(player.pos.X, player.pos.Y.Clamp(25, 375)), player.vel, player.view);
         }
 
-        private GameObject PlayerVeloUpdate(GameObject player, int dir)
+        private static GameObject PlayerVeloUpdate(GameObject player, int dir)
         {
-            return new GameObject(player.pos, new Vector(0, 200 * dir));
+            return new GameObject(player.pos, new Vector(0, 200 * dir), player.view);
         }
 
-        private GameObject PhysicsUpdate(TimeSpan timeSpan, GameObject gameObject)
+        private static GameObject PhysicsUpdate(TimeSpan timeSpan, GameObject gameObject)
         {
-            return new GameObject(gameObject.pos + gameObject.vel * timeSpan.TotalSeconds, gameObject.vel);
+            return new GameObject(gameObject.pos + gameObject.vel * timeSpan.TotalSeconds, gameObject.vel, gameObject.view);
         }
 
-        private GameObject BallInitial()
-        {
-            return new GameObject(new Point(dxBoardHalf, dyBoardHalf), new Vector(200, -200));
-        }
-
-        private GameObject UpdateBall(TimeSpan timeSpan, GameObject ball, GameObject p1, GameObject p2, GameState gameState)
+        private static GameObject UpdateBall(TimeSpan timeSpan, GameObject ball, GameObject p1, GameObject p2, GameState gameState)
         {
             if (gameState == GameState.Paused)
                 return ball;
 
             if (!Within(-1, dxBoard, (int)ball.pos.X))
-                return new GameObject(new Point(dxBoardHalf, dyBoardHalf), ball.vel);
+                return new GameObject(new Point(dxBoardHalf, dyBoardHalf), ball.vel, ball.view);
 
             return PhysicsUpdate(timeSpan, BallVeloUpdate(ball, p1, p2));
         }
 
-        private GameObject BallVeloUpdate(GameObject ball, GameObject p1, GameObject p2)
+        private static GameObject BallVeloUpdate(GameObject ball, GameObject p1, GameObject p2)
         {
             return new GameObject(ball.pos,
                 new Vector(
                     StepV(ball.vel.X, Within(p2, ball, -10), Within(p1, ball, 10)),
-                    StepV(ball.vel.Y, ball.pos.Y > dyBoard - 5, ball.pos.Y < 5)));
+                    StepV(ball.vel.Y, ball.pos.Y > dyBoard - 5, ball.pos.Y < 5)), 
+                ball.view);
         }
 
-        private double StepV(double v, bool lowerCollision, bool upperCollision)
+        private static double StepV(double v, bool lowerCollision, bool upperCollision)
         {
             if (lowerCollision)
                 return -Math.Abs(v);
@@ -209,7 +180,7 @@ namespace SodiumPong
             return v;
         }
 
-        private bool Within(GameObject p, GameObject b, int xOffset)
+        private static bool Within(GameObject p, GameObject b, int xOffset)
         {
             int tmp = (int)(p.pos.X + xOffset);
             int right = (int)p.pos.X;
@@ -220,9 +191,15 @@ namespace SodiumPong
                    Within((int)(p.pos.Y - 27.5), (int)(p.pos.Y + 27.5), (int)b.pos.Y);
         }
 
-        private bool Within(int left, int right, int n)
+        private static bool Within(int left, int right, int n)
         {
             return left < n && n < right;
+        }
+
+        private static void Draw(GameObject gameObject)
+        {
+            Canvas.SetLeft(gameObject.view, gameObject.pos.X);
+            Canvas.SetTop(gameObject.view, gameObject.pos.Y);
         }
 
         private static Rectangle RectanglePlayer()
@@ -282,15 +259,27 @@ namespace SodiumPong
             return textBlock;
         }
 
+        private static GameObject PlayerInitial(Point pointInitial, UIElement view)
+        {
+            return new GameObject(pointInitial, new Vector(0, 0), view);
+        }
+
+        private static GameObject BallInitial(UIElement view)
+        {
+            return new GameObject(new Point(dxBoardHalf, dyBoardHalf), new Vector(200, -200), view);
+        }
+
         internal class GameObject
         {
             internal readonly Point pos;
             internal readonly Vector vel;
+            internal readonly UIElement view;
 
-            public GameObject(Point pos, Vector vel)
+            public GameObject(Point pos, Vector vel, UIElement view)
             {
                 this.pos = pos;
                 this.vel = vel;
+                this.view = view;
             }
         }
     }
