9070ee6cd29d8526c89242732929d5b2cb7e875f
Robert Walter <Robert Walter>
2016-04-08 13:58:59 +0200

    Added LiveScripting example

---
+++ LiveScripting/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using Sodium;
+
+namespace LiveScripting
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtInput.AcceptsTab = true;
+            txtInput.AcceptsReturn = true;
+            txtInput.TextWrapping = TextWrapping.Wrap;
+
+            Transaction.RunVoid(() =>
+            {
+                var sTextChanged = new StreamSink<string>();
+
+                txtInput.TextChanged += (sender, args) =>
+                {
+                    var textCur = (args.Source as TextBox)?.Text;
+                    txtInput.Dispatcher.InvokeAsync(() => sTextChanged.Send(textCur));
+                };
+
+                Cell<ScriptStateExtended> cScriptState = sTextChanged.Accum(ScriptStateExtended.Nil,
+                    (code, sse) => Execute(code, sse.scriptState));
+
+                cScriptState.Listen(sse =>
+                    txtResult.Text = sse.cee == null
+                        ? sse.scriptState?.Result?.ReturnValue?.ToString()
+                        : sse.cee.Message
+                );
+            });
+        }
+
+        private static ScriptStateExtended Execute(string code, Task<ScriptState<object>> scriptState)
+        {
+            try
+            {
+                if (scriptState == null)
+                    return new ScriptStateExtended(CSharpScript.RunAsync(code), null);
+
+                return new ScriptStateExtended(scriptState.Result.ContinueWithAsync(code), null);
+            }
+            catch (CompilationErrorException cee)
+            {
+                return new ScriptStateExtended(scriptState, cee);
+            }
+        }
+
+        private struct ScriptStateExtended
+        {
+            internal readonly Task<ScriptState<object>> scriptState;
+            internal readonly CompilationErrorException cee;
+            internal static readonly ScriptStateExtended Nil = new ScriptStateExtended(null, null);
+
+            internal ScriptStateExtended(Task<ScriptState<object>> scriptState, CompilationErrorException cee)
+            {
+                this.scriptState = scriptState;
+                this.cee = cee;
+            }
+        }
+    }
+}
