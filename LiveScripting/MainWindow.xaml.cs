d6d8f6af3d1d99801c828f311a525ec2c6d153bf
Robert Walter <Robert Walter>
2016-04-09 18:20:22 +0200

    Added comments and some minor changes to functions

--- LiveScripting/MainWindow.xaml.cs
+++ LiveScripting/MainWindow.xaml.cs
@@ -11,22 +11,29 @@ namespace LiveScripting
 {
     public partial class MainWindow : Window
     {
+        private static readonly StreamSink<string> sDocChanged = new StreamSink<string>();
+
         public MainWindow()
         {
             InitializeComponent();
             
             Transaction.RunVoid(() =>
             {
-                var sTextChanged = new StreamSink<string>();
+                // register a handler for document changes; sends new document text to StreamSink sDocChanged 
+                txtInput.Document.Changed += DocumentChangedHandler;
 
-                txtInput.Document.Changed += (sender, args) =>
-                {
-                    var textCur = (sender as TextDocument)?.Text;
-                    txtInput.Dispatcher.InvokeAsync(() => sTextChanged.Send(textCur));
-                };
-
-                Cell<ExecutionResult> cExecutionResult = sTextChanged.Accum(ExecutionResult.Nil, Execute);
+                /**
+                 * we accumulate the state of our C# script on each document change
+                 * we start with a "nil" state
+                 */
+                Cell<ExecutionResult> cExecutionResult = sDocChanged.Accum(ExecutionResult.Nil, Execute);
                 
+                /**
+                 * we interface our FRP logic with WPF
+                 * if the execution result doesn't contain a compiler error, 
+                   we try to retrieve the result of the script and collapse the error message
+                 * if there is an error, we keep showing the last valid result, but highlight the current error
+                 */
                 cExecutionResult.Listen(executionResult =>
                 {
                     if (executionResult.compilerException == null)
@@ -43,18 +50,34 @@ namespace LiveScripting
             });
         }
 
-        private static ExecutionResult Execute(string code, ExecutionResult sse)
+        /**
+         * send document changes to our FRP sink, asynchronously.
+         */
+        private void DocumentChangedHandler(object sender, DocumentChangeEventArgs e)
+        {
+            var textCur = (sender as TextDocument)?.Text;
+            txtInput.Dispatcher.InvokeAsync(() => sDocChanged.Send(textCur));
+        }
+
+        /**
+         * Ran each time the document changes.
+         * Within a session, we keep the scriptstate alive, meaning that you can access variables you declared once but deleted from teh script later.
+         * Thankfully, ContinueWithAsync allows us to override the same identifier.
+         *
+         * In case of a compiler error, we keep the last script state alive and store the compiler error to display it on the screen
+         */
+        private static ExecutionResult Execute(string code, ExecutionResult executionResult)
         {
             try
             {
-                if (sse.taskScriptState == null)
+                if (executionResult.taskScriptState == null)
                     return new ExecutionResult(CSharpScript.RunAsync(code), null);
 
-                return new ExecutionResult(sse.taskScriptState.Result.ContinueWithAsync(code), null);
+                return new ExecutionResult(executionResult.taskScriptState.Result.ContinueWithAsync(code), null);
             }
             catch (CompilationErrorException cee)
             {
-                return new ExecutionResult(sse.taskScriptState, cee);
+                return new ExecutionResult(executionResult.taskScriptState, cee);
             }
         }
 
